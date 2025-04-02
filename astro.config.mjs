136bff60ca3cc41c8f9fadb5910ff990d2a08995
filippocinfriuni <filippo.cinfri.uni@gmail.com>
2025-04-02 11:01:06 +0200

    update of astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "http://blackswanpetter.com/",
   integrations: [mdx(), sitemap(), tailwind()],
